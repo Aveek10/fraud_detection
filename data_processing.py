ca26a0768d837cf2cc69205be8b6b965c5f1d429
Aveek10 <keevayor@gmail.com>
2022-08-26 17:36:36 +0530

    added a new function sum:

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,9 @@
 print("adding all the data prcessing code related to fraud detection")
+
+a=5
+b=7
+def adding (a,b):
+    c=a+b
+    return c
+
+print("return the value of adding a and b",adding(a,b))
