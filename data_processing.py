d6d1aff19fa817d7b3e55fcb7475e68f5a22dff7
Aveek10 <keevayor@gmail.com>
2022-08-26 17:39:56 +0530

    Revert "added a new function sum:"
    This reverts commit ca26a0768d837cf2cc69205be8b6b965c5f1d429.
    
    wrong code inserted

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,1 @@
 print("adding all the data prcessing code related to fraud detection")
-
-a=5
-b=7
-def adding (a,b):
-    c=a+b
-    return c
-
-print("return the value of adding a and b",adding(a,b))
